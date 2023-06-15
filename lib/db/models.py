f5166820d8535fcfdf56eb66d6f778d02cd111e5
Jesse Ilc <jesseilc123@gmail.com>
2023-06-15 11:18:38 -0500

    Added worlds table, no relationships added yet

--- lib/db/models.py
+++ lib/db/models.py
@@ -31,4 +31,21 @@ class Server(Base):
         return f"Server: {self.id}" + \
             f"Server Name: {self.server_name}" + \
             f"Server IP: {self.server_ip}"
+    
+class Worlds(Base):
+    __tablename__ = "worlds"
+
+    id = Column(Integer(), primary_key=True) 
+
+    name = Column(String())
+    seed = Column(Integer())
+    spawn = Column(Integer())
+    player_count = Column(Integer())
+
+    def __repr__(self):
+        return f"World: {self.id}" + \
+            f"Name: {self.name}" + \
+            f"Seed: {self.seed}" + \
+            f"Spawn: {self.spawn}" + \
+            f"Player count: {self.player_count}"
 
