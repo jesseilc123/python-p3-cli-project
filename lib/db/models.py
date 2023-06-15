83891704d5d51844fd47d19088fad93628b0a88e
Jesse Ilc <jesseilc123@gmail.com>
2023-06-15 11:15:12 -0500

    Created servers table

--- lib/db/models.py
+++ lib/db/models.py
@@ -7,6 +7,7 @@ class Player(Base):
     __tablename__ = "players"
 
     id = Column(Integer(), primary_key=True)
+
     user_name = Column(String())
     role = Column(String())
     exp = Column(Integer())
@@ -18,5 +19,16 @@ class Player(Base):
             f"Experience: {self.exp}" + \
             f"Skin: {self.skin}"
 
+class Server(Base):
+    __tablename__ = "servers"
 
+    id = Column(Integer(), primary_key=True)
+
+    server_name = Column(String())
+    server_ip = (Integer())
+
+    def __repr__(self):
+        return f"Server: {self.id}" + \
+            f"Server Name: {self.server_name}" + \
+            f"Server IP: {self.server_ip}"
 
