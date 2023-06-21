1ce1c6ea9b03b1f34d32217a71637b210a4580e0
Jesse Ilc <jesseilc123@gmail.com>
2023-06-21 15:16:45 -0500

    Submenu's working as intended, working on getting data to be displayed

--- lib/cli.py
+++ lib/cli.py
@@ -4,6 +4,4 @@ if __name__ == '__main__':
     print("***Welcome to my Mincraft Database!***")
     main_menu()
 
-# options for users:
-# Servers, players, worlds
 # look up, display all or add 
--- lib/helpers.py
+++ lib/helpers.py
@@ -1,3 +1,7 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from db.models import Player, Server, World
+
 def main_menu():
     choice = 0
 
@@ -10,31 +14,31 @@ def main_menu():
     while choice !=4:
         choice = int(input())
         if choice == 1:
-            print("***Player Menu***")
-            print("1) Lookup Player")
-            print("2) Display all Players")
-            print("3) Back to Main Menu")
+            sub_menu("Player")
         if choice == 2:
-            print("***World Menu***")
-            print("1) Lookup World")
-            print("2) Display all Worlds")
-            print("3) Back to Main Menu")
+            sub_menu("World")
         if choice == 3:
-            print("***Server Menu***")
-            print("1) Lookup Server")
-            print("2) Display all Servers")
-            print("3) Back to Main Menu")
+            sub_menu("Server")
         if choice == 4:
             print("Quiting Program")
-            print("Program Terminated")
+    print("Program Terminated")
 
-def sub_menu():
+def sub_menu(category):
     sub_choice = 0
-    sub_choice = int(input())
-    if sub_choice == 1:
-        print("test for look up")
-    if sub_choice == 2:
-        print("test for display")
-    if sub_choice == 3:
-        print("test for back")
+    while sub_choice != 3:
+        print(f"***{category} Menu***")
+        print(f"1) Lookup {category}")
+        print(f"2) Display all {category}s")
+        print("3) Back to Main Menu")
+        sub_choice = int(input())
+
+        if sub_choice == 1:
+            print("test for look up")
+            item = session.query()
+        if sub_choice == 2:
+            print("test for display")
+        if sub_choice == 3:
+            print("test for back")
+    main_menu()
+
     
