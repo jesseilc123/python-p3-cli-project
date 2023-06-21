7f9be6bb0731177325d2383b87687cb35457828a
Jesse Ilc <jesseilc123@gmail.com>
2023-06-21 15:07:39 -0500

    added main menu to cli.py, now working on navigating between submenu's and main menu

--- lib/cli.py
+++ lib/cli.py
@@ -1,2 +1,9 @@
+from helpers import *
+
 if __name__ == '__main__':
-    print("Welcome to the my Mincraft Database!")
+    print("***Welcome to my Mincraft Database!***")
+    main_menu()
+
+# options for users:
+# Servers, players, worlds
+# look up, display all or add 
--- lib/helpers.py
+++ lib/helpers.py
@@ -1,9 +1,40 @@
 def main_menu():
     choice = 0
 
-    while choice !=3:
-        print("***Main Menu***")
-        print("1) Player menu")
-        print("2) World menu")
-        print("3) Server menu")
-        print("4) Exit")
+    print("***Enter the corresponding number***")
+    print("1) Player menu")
+    print("2) World menu")
+    print("3) Server menu")
+    print("4) Exit")
+
+    while choice !=4:
+        choice = int(input())
+        if choice == 1:
+            print("***Player Menu***")
+            print("1) Lookup Player")
+            print("2) Display all Players")
+            print("3) Back to Main Menu")
+        if choice == 2:
+            print("***World Menu***")
+            print("1) Lookup World")
+            print("2) Display all Worlds")
+            print("3) Back to Main Menu")
+        if choice == 3:
+            print("***Server Menu***")
+            print("1) Lookup Server")
+            print("2) Display all Servers")
+            print("3) Back to Main Menu")
+        if choice == 4:
+            print("Quiting Program")
+            print("Program Terminated")
+
+def sub_menu():
+    sub_choice = 0
+    sub_choice = int(input())
+    if sub_choice == 1:
+        print("test for look up")
+    if sub_choice == 2:
+        print("test for display")
+    if sub_choice == 3:
+        print("test for back")
+    
