628e1f5fce0fca60a219d77606c2106b42bbb943
Jesse Ilc <jesseilc123@gmail.com>
2023-06-21 18:10:35 -0500

    Added submenu which displays from database and allows look up by name

--- lib/cli.py
+++ lib/cli.py
@@ -1,7 +1,5 @@
 from helpers import *
 
 if __name__ == '__main__':
-    print("***Welcome to my Mincraft Database!***")
+    print("***Welcome to my Minecraft Database***")
     main_menu()
-
-# look up, display all or add 
--- lib/helpers.py
+++ lib/helpers.py
@@ -1,10 +1,14 @@
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from db.models import Player, Server, World
 
+engine = create_engine("sqlite:///db/minecraft_model.db")
+Session = sessionmaker(bind=engine)
+session = Session()
+
 def main_menu():
     choice = 0
-
     print("***Enter the corresponding number***")
     print("1) Player menu")
     print("2) World menu")
@@ -14,31 +18,45 @@ def main_menu():
     while choice !=4:
         choice = int(input())
         if choice == 1:
-            sub_menu("Player")
+            sub_menu("player", Player)
         if choice == 2:
-            sub_menu("World")
+            sub_menu("world", World)
         if choice == 3:
-            sub_menu("Server")
+            sub_menu("server", Server)
         if choice == 4:
-            print("Quiting Program")
+            print("Quiting Program...")
     print("Program Terminated")
+    quit()
 
-def sub_menu(category):
+def sub_menu(category, data_list):
     sub_choice = 0
     while sub_choice != 3:
-        print(f"***{category} Menu***")
-        print(f"1) Lookup {category}")
+        print(f"***{category.title()} Menu***")
         print(f"2) Display all {category}s")
+        print(f"1) Lookup {category} by Name")
         print("3) Back to Main Menu")
         sub_choice = int(input())
 
         if sub_choice == 1:
-            print("test for look up")
-            item = session.query()
+            for data in session.query(data_list).all():
+                print(data)
         if sub_choice == 2:
-            print("test for display")
+            search_name = input("Please type the name...")
+            if category == "player":
+                print(session.query(data_list).filter(data_list.user_name==search_name).first())
+            elif category == "world":
+                print(session.query(data_list).filter(data_list.name==search_name).first())
+            elif category == "server":
+                print(session.query(data_list).filter(data_list.server_name==search_name).first())
         if sub_choice == 3:
             print("test for back")
     main_menu()
 
-    
+def add_to_player():
+    pass
+
+def add_to_world():
+    pass
+
+def add_to_server():
+    pass
